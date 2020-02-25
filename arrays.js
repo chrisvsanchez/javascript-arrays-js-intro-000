61065a4a79f107447ef9d04af9017f53127c33e6
Chris Sanchez <chrisvsanchez@gmail.com>
2020-02-25 03:41:59 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,10 +35,7 @@ function removeElementFromBeginningOfArray(array){
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
-  return array.pop();
+ array.pop();
+ return array;
 }
 
-function destructivelyRemoveElementFromEndOfArray(array){
-  array.slice(0, array.length-1);
-  return array;
-}
